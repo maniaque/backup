a639622f145697221e4de03497ff039f17c37363
Leo Nikolaev <l-nikolaev@iarco.org>
2016-12-16 09:48:24 +0300

    cron, install

---
+++ loops.bash
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# for f in $(ls /); do
+	# echo $f
+# done
+
+# for f in $(ls /); do echo $f; done
+
+# COUNT=7
+# while [ $COUNT -gt 0 ]; do
+	# echo $COUNT
+	
+	# # let COUNT=COUNT-1
+	# COUNT=$(( $COUNT-1 ))
+# done
+
+
+# for (( i=0; i < 10; i++ )); do
+	# echo $i
+# done
+
+# for i in 5 18 19 21 16; do
+	# echo $i
+# done
+
+# for i in $(seq 1 .2 3); do
+	# echo $i
+# done
+
+# for (( i=0, j=10; i != j; i++, j-- )); do
+	# echo $(( i*j ))
+# done
+
+# for i in 5 18 19 21 16; do
+	# if [ $i -gt 10 -a $i -lt 20 ]; then
+		# continue
+	# fi
+
+	# echo $i
+	
+	# if [ $i -eq 18 ]; then
+	# if [ "$i" == "18" ]; then
+		# break
+	# fi
+# done
+
+
+
